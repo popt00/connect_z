98b17604ac0b12d06489ce9be8c525e3830ef0cd
Parimal Donga <parimaldonga00@gmail.com>
2023-04-04 20:56:31 -0400

    updated fetch.java
    Tried fetching anlist user animelist and is going in one big string so need to update this code to fetch user completed, watching, paused and dropped list into arraylist with anime title, user rating and anime link

--- src/series_rating/Fetch.java
+++ src/series_rating/Fetch.java
@@ -12,13 +12,20 @@ import java.net.*;
 import java.io.*;
 public class Fetch {
     public static void main(String[] args) throws IOException{
-    	URL oracle = new URL("https://myanimelist.net/animelist/Rimusimp?status=2");
+    	URL oracle = new URL("https://anilist.co/user/popt/animelist");
         BufferedReader in = new BufferedReader(
         new InputStreamReader(oracle.openStream()));
 
         String inputLine;
-        while ((inputLine = in.readLine()) != null)
+        int total_new_line=0;
+        while ((inputLine = in.readLine()) != null) {
+        	for(int i=0;i<inputLine.length();i++) {
+        		if(inputLine.charAt(i)=='\n')total_new_line++;
+        	}
             System.out.println(inputLine);
+        }
+        System.out.println("new line:");
+        System.out.println(total_new_line);
         in.close();
     }
 }
